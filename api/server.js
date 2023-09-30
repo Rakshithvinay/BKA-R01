88d2481f92255b608806ced6267ab2c7d288d790
Rakshithvinay <vinayrakshith96@gmail.com>
2023-09-30 10:45:16 +0530

    RFINAL

--- api/server.js
+++ api/server.js
@@ -11,8 +11,6 @@ import cors from "cors";
 
 
 
-
-
 const app = express()
 dotenv.config()
 
@@ -35,7 +33,6 @@ mongoose.connection.on("disconnected", () => {
 app.use(cors())
 app.use(cookieParser())
 app.use(express.json());
-
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", usersRoute);
